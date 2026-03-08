c953e11f88b1d90bf570295227afa9575a6cc2c5
Alex <alex.tech.lab@outlook.com>
2026-03-08 23:17:20 +0800

    [refactor] 清理 DeviceUtils 中未使用的设备类型判断属性

--- openclaw-deck-swift/openclaw-deck-swift/Utils/DeviceUtils.swift
+++ openclaw-deck-swift/openclaw-deck-swift/Utils/DeviceUtils.swift
@@ -7,6 +7,8 @@ import Foundation
 
 #if os(iOS)
     import UIKit
+#elseif os(macOS)
+    import AppKit
 #endif
 
 /// 设备类型枚举
@@ -19,34 +21,13 @@ enum DeviceType {
 
 /// 设备判断工具
 enum DeviceUtils {
-    /// 当前设备类型
-    static var currentType: DeviceType {
-        #if os(iOS)
-            if UIDevice.current.userInterfaceIdiom == .pad {
-                return .iPad
-            } else {
-                return .iPhone
-            }
-        #elseif os(macOS)
-            return .mac
-        #else
-            return .unknown
-        #endif
-    }
-
-    /// 是否为 iPad
-    static var isIPad: Bool {
-        currentType == .iPad
-    }
-
-    /// 是否为 iPhone
-    static var isIPhone: Bool {
-        currentType == .iPhone
-    }
-
     /// 是否为 Mac
     static var isMac: Bool {
-        currentType == .mac
+        #if os(macOS)
+            return true
+        #else
+            return false
+        #endif
     }
 
     /// 是否为移动设备（iPhone 或 iPad）
